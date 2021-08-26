5a6f30b8b16f024b2572710668ad470d2aea87f5
gaoguodong <gaoguodong@buzzbreaknews.com>
2021-08-26 14:22:35 +0800

    add async flag

--- model-engine-v2.js
+++ model-engine-v2.js
@@ -342,12 +342,12 @@ class ModelEngineV2 {
     }
 }
 
-async function getCollection(module, tableMaster, tablePort, dbMaster, dbPort, redisGetAsync) {
+async function getCollectionAsync(module, tableMaster, tablePort, dbMaster, dbPort, redisGetAsync) {
     const rdFlag = await redisGetAsync(`switch:${module}:rd:${tablePort}}`)
     return new ModelEngineV2(module, tableMaster, tablePort, dbMaster, dbPort, redisGetAsync, rdFlag)
 }
 
 module.exports = {
     ModelEngineV2,
-    getCollection,
+    getCollectionAsync,
 }
